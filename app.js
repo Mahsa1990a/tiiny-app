713bd771ca4e1ddc4a591802c1bbc12881555d1b
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-26 14:38:05 +0000

    added comments about non-blocking functions(creatServer & listen)

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const PORT = 8080;
 // requestHandler: a callback function which handles requests and sends response\
 // We read values from the request, and send a string back to the client using the response object
 const requestHandler = function(request, response) {
+
+  console.log('In requestHandler');
   response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
 };
 
-const server = http.createServer(requestHandler);
+const server = http.createServer(requestHandler);    // createServer and listen funtions are non-blocking and will return immediately
+console.log('Server created');
 
 server.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
+
+console.log('Last line (after .listen call)');
