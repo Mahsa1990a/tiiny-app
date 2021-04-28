3b266bb0670dc8634d3445dfd2995b96a1fc06d7
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-28 20:59:36 +0000

    updated test

--- test.js
+++ test.js
@@ -52,3 +52,11 @@ user = {
 };
 
 console.log(users);
+/////////////////////////////////////////////////////////////////
+
+const urlDatabase1 = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+};
+
+console.log(urlDatabase1.b6UTxQ.longURL)
